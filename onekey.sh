b410f262a5c03e1b27b4f8fa1785bef41154dc88
sinian-liu <86273555+sinian-liu@users.noreply.github.com>
2025-01-19 18:00:25 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -1,58 +1,64 @@
 #!/bin/bash
 
+# 设置颜色
+GREEN="\033[32m"
+RED="\033[31m"
+YELLOW="\033[33m"
+RESET="\033[0m"
+
 # 设置快捷命令 s
 if ! grep -q "alias s=" ~/.bashrc; then
-    echo "正在为 s 设置快捷命令..."
+    echo -e "${GREEN}正在为 s 设置快捷命令...${RESET}"
     echo "alias s='wget -O /root/onekey.sh https://github.com/sinian-liu/onekey/raw/main/onekey.sh && chmod +x /root/onekey.sh && /root/onekey.sh'" >> ~/.bashrc
     source ~/.bashrc
-    echo "快捷命令 s 已设置。"
+    echo -e "${GREEN}快捷命令 s 已设置。${RESET}"
 else
-    echo "快捷命令 s 已经存在。"
+    echo -e "${YELLOW}快捷命令 s 已经存在。${RESET}"
 fi
 
 # 提示用户输入选项
-echo "============================================="
-echo "服务器推荐：https://my.frantech.ca/aff.php?aff=4337"
-echo "VPS评测官方网站：https://www.1373737.xyz/"
-echo "YouTube频道：https://www.youtube.com/@cyndiboy7881"
-echo "============================================="
+echo -e "${GREEN}=============================================${RESET}"
+echo -e "${GREEN}服务器推荐：https://my.frantech.ca/aff.php?aff=4337${RESET}"
+echo -e "${GREEN}VPS评测官方网站：https://www.1373737.xyz/${RESET}"
+echo -e "${GREEN}YouTube频道：https://www.youtube.com/@cyndiboy7881${RESET}"
+echo -e "${GREEN}=============================================${RESET}"
 echo "请选择要执行的操作："
-echo "1. 安装 v2ray 脚本"
-echo "2. VPS 一键测试脚本"
-echo "3. BBR 安装脚本"
-echo "4. 一键永久禁用 IPv6"
-echo "5. 一键解除禁用 IPv6"
-echo "6. 无人直播云 SRS 安装"
-echo "7. 宝塔纯净版安装"
-echo "8. 长时间保持 SSH 会话连接不断开"
-echo "9. 重启服务器"
-echo "============================================="
+echo -e "${YELLOW}1. 安装 v2ray 脚本${RESET}"
+echo -e "${YELLOW}2. VPS 一键测试脚本${RESET}"
+echo -e "${YELLOW}3. BBR 安装脚本${RESET}"
+echo -e "${YELLOW}4. 一键永久禁用 IPv6${RESET}"
+echo -e "${YELLOW}5. 一键解除禁用 IPv6${RESET}"
+echo -e "${YELLOW}6. 无人直播云 SRS 安装${RESET}"
+echo -e "${YELLOW}7. 宝塔纯净版安装${RESET}"
+echo -e "${YELLOW}8. 长时间保持 SSH 会话连接不断开${RESET}"
+echo -e "${YELLOW}9. 重启服务器${RESET}"
+echo -e "${GREEN}=============================================${RESET}"
 read -p "请输入选项 [1-9]:" option
 
 case $option in
     1)
         # 安装 v2ray 脚本
-        echo "正在安装 v2ray ..."
+        echo -e "${GREEN}正在安装 v2ray ...${RESET}"
         wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/sinian-liu/v2ray-agent-2.5.73/master/install.sh" && chmod 700 /root/install.sh && /root/install.sh
         ;;
     2)
         # VPS 一键测试脚本
-        echo "正在进行 VPS 测试 ..."
+        echo -e "${GREEN}正在进行 VPS 测试 ...${RESET}"
         bash <(curl -sL https://raw.githubusercontent.com/sinian-liu/VPStest/main/system_info.sh)
         ;;
     3)
         # BBR 安装脚本
-        echo "正在安装 BBR ..."
+        echo -e "${GREEN}正在安装 BBR ...${RESET}"
         wget -O tcpx.sh "https://github.com/sinian-liu/Linux-NetSpeed-BBR/raw/master/tcpx.sh" && chmod +x tcpx.sh && ./tcpx.sh
         ;;
     4)
         # 永久禁用 IPv6
-        echo "正在禁用 IPv6 ..."
+        echo -e "${GREEN}正在禁用 IPv6 ...${RESET}"
         
         # 检测系统类型（Ubuntu/Debian 或 CentOS/RHEL）
         if [ -f /etc/lsb-release ]; then
             # Ubuntu 或 Debian 系统
-            echo "禁用 IPv6："
+            echo -e "${GREEN}禁用 IPv6：${RESET}"
             sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
             sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
             echo "net.ipv6.conf.all.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
@@ -60,25 +66,25 @@ case $option in
             sudo sysctl -p
         elif [ -f /etc/redhat-release ]; then
             # CentOS 或 RHEL 系统
-            echo "禁用 IPv6："
+            echo -e "${GREEN}禁用 IPv6：${RESET}"
             sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
             sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
             echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
             echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
             sudo sysctl -p
         else
-            echo "无法识别您的操作系统，无法禁用 IPv6。"
+            echo -e "${RED}无法识别您的操作系统，无法禁用 IPv6。${RESET}"
         fi
-        echo "IPv6 已禁用。"
+        echo -e "${GREEN}IPv6 已禁用。${RESET}"
         ;;
     5)
         # 解除禁用 IPv6
-        echo "正在解除禁用 IPv6 ..."
+        echo -e "${GREEN}正在解除禁用 IPv6 ...${RESET}"
         
         # 检测系统类型（Ubuntu/Debian 或 CentOS/RHEL）
         if [ -f /etc/lsb-release ]; then
             # Ubuntu 或 Debian 系统
-            echo "启用 IPv6："
+            echo -e "${GREEN}启用 IPv6：${RESET}"
             sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
             sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
             sudo sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
@@ -86,20 +92,20 @@ case $option in
             sudo sysctl -p
         elif [ -f /etc/redhat-release ]; then
             # CentOS 或 RHEL 系统
-            echo "启用 IPv6："
+            echo -e "${GREEN}启用 IPv6：${RESET}"
             sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
             sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
             sudo sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
             sudo sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
             sudo sysctl -p
         else
-            echo "无法识别您的操作系统，无法解除禁用 IPv6。"
+            echo -e "${RED}无法识别您的操作系统，无法解除禁用 IPv6。${RESET}"
         fi
-        echo "IPv6 已解除禁用。"
+        echo -e "${GREEN}IPv6 已解除禁用。${RESET}"
         ;;
     6)
         # 无人直播云 SRS 安装
-        echo "正在安装无人直播云 SRS ..."
+        echo -e "${GREEN}正在安装无人直播云 SRS ...${RESET}"
         
         # 提示用户输入直播端口号
         read -p "请输入要使用的直播端口号 (默认为1935): " live_port
@@ -122,7 +128,7 @@ case $option in
         # 检查直播端口是否被占用
         check_port $live_port
         if [ $? -eq 1 ]; then
-            echo "端口 $live_port 已被占用！"
+            echo -e "${RED}端口 $live_port 已被占用！${RESET}"
             read -p "请输入一个新的直播端口号 (默认为1935): " new_live_port
             new_live_port=${new_live_port:-1935}
             live_port=$new_live_port
@@ -131,7 +137,7 @@ case $option in
         # 检查管理端口是否被占用
         check_port $mgmt_port
         if [ $? -eq 1 ]; then
-            echo "端口 $mgmt_port 已被占用！"
+            echo -e "${RED}端口 $mgmt_port 已被占用！${RESET}"
             read -p "请输入一个新的管理端口号 (默认为2022): " new_mgmt_port
             new_mgmt_port=${new_mgmt_port:-2022}
             mgmt_port=$new_mgmt_port
@@ -143,15 +149,26 @@ case $option in
         docker run --restart always -d --name srs-stack -it -p $live_port:$live_port/tcp -p 1935:1935/tcp -p 1985:1985/tcp \
           -p 8080:8080/tcp -p 8000:8000/udp -p 10080:10080/udp \
           -v $HOME/db:/data ossrs/srs-stack:5
-        echo "默认登录地址：http://你的服务器ip:$mgmt_port/mgmt"
+        echo -e "${GREEN}默认登录地址：http://你的服务器ip:$mgmt_port/mgmt${RESET}"
         ;;
     7)
         # 宝塔纯净版安装
-        echo "正在安装宝塔 ..."
-        # 安装脚本
+        echo -e "${GREEN}正在安装宝塔纯净版...${RESET}"
+        
+        # 检查端口是否占用
+        check_port 8888
+        if [ $? -eq 1 ]; then
+            echo -e "${RED}端口 8888 已被占用！${RESET}"
+            read -p "请输入一个新的端口号用于宝塔 (默认使用 8888): " new_baota_port
+            new_baota_port=${new_baota_port:-8888}
+        fi
+
+        # 安装命令
+        curl -sSO https://raw.githubusercontent.com/bootyun/bt_install/master/bt_install.sh && bash bt_install.sh
         ;;
     8)
-        # 长时间保持 SSH 会话连接不断开
+        # 配置 SSH 保持连接
+        echo -e "${GREEN}正在配置 SSH 保持连接...${RESET}"
         read -p "请输入每次心跳请求的间隔时间（单位：分钟，默认为5分钟）： " interval
         interval=${interval:-5}  # 默认值为5分钟
         read -p "请输入客户端最大无响应次数（默认为50次）： " max_count
@@ -167,14 +184,14 @@ case $option in
         echo "正在重启 SSH 服务以应用配置..."
         sudo systemctl restart sshd
 
-        echo "配置完成！心跳请求间隔为 $interval 分钟，最大无响应次数为 $max_count。"
+        echo -e "${GREEN}配置完成！心跳请求间隔为 $interval 分钟，最大无响应次数为 $max_count。${RESET}"
         ;;
     9)
         # 重启服务器
-        echo "正在重启服务器..."
+        echo -e "${GREEN}正在重启服务器...${RESET}"
         sudo reboot
         ;;
     *)
-        echo "无效选项，请重新输入！"
+        echo -e "${RED}无效的选项，请重新选择！${RESET}"
         ;;
 esac
