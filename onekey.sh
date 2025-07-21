771c137f26fd5ced8cdacf4f8f2c1f7b6d6eaba0
sinian-liu <86273555+sinian-liu@users.noreply.github.com>
2025-07-21 16:34:11 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -149,7 +149,7 @@ show_menu() {
         echo -e "${YELLOW}13.安装Windows或Linux系统${RESET}"
         echo -e "${YELLOW}14.服务器对服务器文件传输${RESET}"
         echo -e "${YELLOW}15.安装探针并绑定域名${RESET}"
-        echo -e "${YELLOW}16.共用端口（反代）${RESET}"
+        echo -e "${YELLOW}16.共用端口（反代NPM）${RESET}"
         echo -e "${YELLOW}17.安装 curl 和 wget${RESET}"
         echo -e "${YELLOW}18.Docker安装和管理${RESET}"
         echo -e "${YELLOW}19.SSH 防暴力破解检测${RESET}"
